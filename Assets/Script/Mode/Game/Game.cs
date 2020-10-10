fe4a9bcae457304017654e184caf96df0eb22f93
Félix <felix.gagne.cliche@gmail.com>
2020-10-10 09:49:27 -0400

    add Game class

---
+++ Assets/Script/Mode/Game/Game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Harmony;
+using UnityEngine;
+
+namespace Game
+{
+  public class Game : MonoBehaviour
+  {
+    private InputActions.GameActions gameInputs;
+
+    private void Awake()
+    {
+      gameInputs = Finder.Inputs.Actions.Game;
+    }
+
+    private void Start()
+    {
+      gameInputs.Enable();
+    }
+
+    private void Update()
+    {
+      if (gameInputs.Exit.triggered) ApplicationExtensions.Quit();
+    }
+  }
+}
