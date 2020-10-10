7ce27af2575ee0d6c780835fbae233559d626ba2
Félix <felix.gagne.cliche@gmail.com>
2020-10-10 11:01:31 -0400

    add MainController & add DOTween and SceneBundleLoader

---
+++ Assets/Script/Mode/Main/Main.cs
@@ -0,0 +1,21 @@
+﻿using Harmony;
+using UnityEngine;
+using DG.Tweening;
+
+namespace Game
+{
+  public class Main : MonoBehaviour
+  {
+    [Header("Scenes")] 
+    [SerializeField] private SceneBundle gameScenes;
+
+    private SceneBundleLoader loader;
+
+    private void Awake()
+    {
+      loader = Finder.SceneBundleLoader;
+      DOTween.Init(false, true, LogBehaviour.ErrorsOnly);
+      DOTween.SetTweensCapacity(200, 125);
+    }
+  }
+}
