d5de8ce48b178f945d4c75c0778f95e2120ea22d
Nicolas Bisson <nicolasbisson14@gmail.com>
2020-10-10 18:21:39 -0400

    Rune zone + handler

---
+++ Assets/Script/Rune/RuneZone.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class RuneZone : MonoBehaviour
+{
+        [SerializeField] private Rune runeToSpawn;
+        [SerializeField] private int amountToSpawn = 25;
+
+        private Rune[] spawnedRunes;
+        private float minX;
+        private float maxX;
+        private float minY;
+        private float maxY;
+
+        private void Start()
+        {
+                spawnedRunes = new Rune[amountToSpawn];
+                float halfX = transform.localScale.x / 2;
+                float halfY = transform.localScale.y / 2;
+                minX = transform.position.x - halfX;
+                maxX = transform.position.x + halfX;
+                minY = transform.position.y - halfY;
+                maxY = transform.position.y + halfY;
+                ResetRunes();
+        }
+
+        public void ResetRunes()
+        {
+                foreach (Rune rune in spawnedRunes)
+                {
+                        if (rune != null)
+                                Destroy(rune.gameObject);
+                }
+
+                for (int i = 0; i < amountToSpawn; i++)
+                {
+                        spawnedRunes[i] = Instantiate(runeToSpawn, new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY)), Quaternion.identity);
+                }
+        }
+}
---
+++ Assets/Script/Rune/RuneZoneHandler.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class RuneZoneHandler : MonoBehaviour
+{ 
+    [SerializeField] private RuneZone smallFireRuneZone;
+    [SerializeField] private RuneZone smallEarthRuneZone;
+    [SerializeField] private RuneZone smallWindRuneZone;
+    [SerializeField] private RuneZone smallWaterRuneZone;
+    [SerializeField] private RuneZone mediumFireRuneZone;
+    [SerializeField] private RuneZone mediumEarthRuneZone;
+    [SerializeField] private RuneZone mediumWindRuneZone;
+    [SerializeField] private RuneZone mediumWaterRuneZone;
+    [SerializeField] private RuneZone largeFireRuneZone;
+    [SerializeField] private RuneZone largeEarthRuneZone;
+    [SerializeField] private RuneZone largeWindRuneZone;
+    [SerializeField] private RuneZone largeWaterRuneZone;
+
+    public void ResetAllRuneZones()
+    {
+        smallFireRuneZone.ResetRunes();
+        smallEarthRuneZone.ResetRunes();
+        smallWindRuneZone.ResetRunes();
+        smallWaterRuneZone.ResetRunes();
+        mediumFireRuneZone.ResetRunes();
+        mediumEarthRuneZone.ResetRunes();
+        mediumWindRuneZone.ResetRunes();
+        mediumWaterRuneZone.ResetRunes();
+        largeFireRuneZone.ResetRunes();
+        largeEarthRuneZone.ResetRunes();
+        largeWindRuneZone.ResetRunes();
+        largeWaterRuneZone.ResetRunes();
+    }
+    
+}
