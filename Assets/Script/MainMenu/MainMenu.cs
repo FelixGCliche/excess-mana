37bfcf72edf3375484153e5562fd37a3b33ddf43
Antony Ménard <antonymenard2014@gmail.com>
2020-10-10 15:53:15 -0400

    Potentiel Thunter Crash

---
+++ Assets/Script/MainMenu/MainMenu.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Script.MainMenu
+{
+    class MainMenu : MonoBehaviour
+    {
+        [SerializeField] Scene GameScene;
+        [SerializeField] Scene SettingsScene;
+        [SerializeField] Scene MenuScene;
+
+        Canvas settingsMenu;
+        Canvas mainMenu;
+
+        public void Start()
+        {
+            settingsMenu = GameObject.Find("SettingsMenu").gameObject.GetComponent<Canvas>();
+            mainMenu = GameObject.Find("MainMenu").gameObject.GetComponent<Canvas>();
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+
+        public void GoToSettings()
+        {
+            mainMenu.enabled = false;
+            settingsMenu.enabled = true;
+        }
+
+        public void GoToGame()
+        {
+            SceneManager.LoadScene(GameScene.name);
+            SceneManager.UnloadSceneAsync(MenuScene.name);
+        }
+
+
+    }
+}
