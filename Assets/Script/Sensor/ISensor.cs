b2c991452a478106bd15e72b1fe3454acaddf8de
Félix <felix.gagne.cliche@gmail.com>
2020-10-10 00:07:18 -0400

    add Sensor interface

---
+++ Assets/Script/Sensor/ISensor.cs
@@ -0,0 +1,10 @@
+﻿namespace Game
+{
+  public interface ISensor<out T>
+  {
+    event SensorEventHandler<T> OnSensedObject;
+    event SensorEventHandler<T> OnUnsensedObject;
+  }
+
+  public delegate void SensorEventHandler<in T>(T otherObject);
+}
